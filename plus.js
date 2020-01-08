b8ba322d72ce1301387e1b88facef405241e109e
Jinzhan <steinitz@qq.com>
2020-01-08 18:19:30 +0800

    Update plus.js

--- plus.js
+++ plus.js
@@ -1,1 +1,8 @@
-
+/**
+* @param {number} max 运算结果的最大值
+*/
+module.exports = function plus(max) {
+  const firstNum = (Math.random() * max ) | 0 + 1;
+  const secondNum = (Math.random() * (max - firstNum))  | 0 + 1;
+  return `${firstNum} + ${secondNum} = `;
+};
